c1c919b5d1bb8d90c49cb50e9b01825cf09cd5c7
hamidrezaramzani <hrra13801396h@gmail.com>
2020-11-26 01:53:05 +0330

    Change Post Item Style

--- client/src/Components/Home/PostItem.js
+++ client/src/Components/Home/PostItem.js
@@ -1,14 +1,15 @@
 import image from "../../images/background.jpg";
-import { FaPen } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
 const PostItem = () => {
   return (
     <div className="post-item">
       <img src={image} alt="post item" />
       <h3>Fuck Your Ideas</h3>
       <p>
-        hamidrezaramzani
-        <FaPen />
+        Novamber 26 , 2020 01:41
+        <FaClock />
       </p>
+      <div className="post-item-category">Music</div>
     </div>
   );
 };
