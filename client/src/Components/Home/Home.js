fce60e14371901ed3685179738280d670d5d2d39
Hamidreza Ramzani <hrra13801396h@gmail.com>
2020-11-30 03:56:54 +0330

    Remove Side Content :(

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,8 @@
 import { Container, Row } from "react-bootstrap";
 import Content from "./Content";
-import HomeContent from "./HomeContent";
 import MainNavbar from "./MainNavbar";
 import MainSlideShow from "./MainSlideShow";
 import PostList from "./PostList";
-import Welcome from "./Welcome";
 const Home = () => {
     return <>
         <Container fluid>
