cc9b70ab30351671cf907e6aa525995918be8262
hamidrezaramzani <hrra13801396h@gmail.com>
2020-12-30 07:12:52 +0330

    Select a valid profile image and set it on profile provider

--- client/src/Components/UserProfile/SelectProfile.js
+++ client/src/Components/UserProfile/SelectProfile.js
@@ -1,13 +1,33 @@
+import swal from "../swal";
+import { ProfileContext } from "../../Context/ProfileProvider";
+import { useContext } from "react";
 const SelectProfile = (props) => {
+  const { setState } = useContext(ProfileContext);
   const getUserProfile = () => {
     const IMAGES_PATH = "http://localhost:8000/public/images/";
     return props.img ? IMAGES_PATH + props.img : IMAGES_PATH + "user.png";
+  };
+  const SUPPORTED_FORMATS = ["png", "jpg", "jpeg", "gif"];
+
+  const selectProfile = (e) => {
+    const file = e.target.files[0];
+    const format = file.name.split(".")[1];
+    if (!SUPPORTED_FORMATS.includes(format.toLowerCase())) {
+      swal.fire({
+        title: "Failed",
+        html: "please select a valid format",
+        icon: "error",
+      });
+      e.target.value = null;
+      return;
+    }
+    setState(file);
   };
   return (
     <div className="select-profile">
       <img src={getUserProfile()} alt="user profile" />
       <br />
-      <input type="file" name="profile"  />
+      <input type="file" onChange={selectProfile} name="profile" />
     </div>
   );
 };
